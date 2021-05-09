f183ec0d077002a1dc29a03bf9b2b5415662d9cc
Brian Sweet <bsweet48@gmail.com>
2021-05-09 16:55:41 -0400

    Initial commit

---
+++ carts/test/main.lua
@@ -0,0 +1,38 @@
+bullets={}
+x=64 y=64 r=0
+function newbullet()
+	r+=0.05
+	if (r>1) then r=0 end
+	add(bullets, {
+		x=x,y=y,
+		dx=sin(r)*2,
+		dy=cos(r)*2
+	})
+end
+function drawbullets()
+  foreach(bullets, function(b) pset(b.x,b.y,7) end)
+end
+function updatebullets()
+	foreach(bullets, 
+		function(b)
+  	b.x += b.dx
+  	b.y += b.dy
+  	if b.x < -20 or b.x > 130
+  	or b.y < -20 or b.y > 130
+  	then del(bullets,b) end
+		end
+	)
+end
+function _update60()
+ updatebullets()
+ if (btn(0) and x > 0) x -= 1
+ if (btn(1) and x < 127) x += 1
+ if (btn(2) and y > 0) y -= 1
+ if (btn(3) and y < 127) y += 1
+	if (time()%0.5 == 0) then newbullet() end
+end
+function _draw()
+	cls(5)
+	circfill(x, y, 7, 14)
+	drawbullets()
+end
