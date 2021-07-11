6f282bc61eadbb2d506118eebafa96373b3d73b4
Brian Sweet <bsweet48@gmail.com>
2021-07-11 13:21:25 -0400

    Restarted game.

--- carts/test/main.lua
+++ carts/test/main.lua
@@ -1,47 +1,35 @@
-bullets={}
-x=64 y=64 r=0
-function newbullet()
-	r+=0.05
-	if (r>1) then r=0 end
-	add(bullets, {
-		x=x,y=y,
-		dx=sin(r)*2,
-		dy=cos(r)*2
-	})
-end
-function drawbullets()
-  foreach(bullets, function(b) pset(b.x,b.y,7) end)
-end
-function updatebullets()
-	foreach(bullets, 
-		function(b)
-  			b.x += b.dx
-  			b.y += b.dy
-  			if b.x < -20 or b.x > 130
-  				or b.y < -20 or b.y > 130
-  			then del(bullets,b) end
-		end
-	)
-end
-function overlap(a,b)
-	return not (a.x>b.x+b.w 
-		   or a.y>b.y+b.h 
-		   or a.x+a.w<b.x 
-		   or a.y+a.h<b.y)
-end
-function input()
-	if (btn(0) and x > 0) x -= 1
-	if (btn(1) and x < 127) x += 1
-	if (btn(2) and y > 0) y -= 1
-	if (btn(3) and y < 127) y += 1
-end
-function _update60()
-	updatebullets()
-	input()
-	if (time()%0.5 == 0) then newbullet() end
+function drawbackground(linecol)
+	-- small
+	line(62, 59, 65, 59, linecol) -- N
+	line(65, 59, 68, 62, linecol) -- NE
+	line(68, 62, 68, 65, linecol) -- E
+	line(68, 65, 65, 68, linecol) -- SE
+	line(62, 68, 65, 68, linecol) -- S
+	line(62, 68, 59, 65, linecol) -- SW
+	line(59, 62, 59, 65, linecol) -- W
+	line(59, 62, 62, 59, linecol) -- NW
+
+	-- dividers
+	line(65, 59, 90, 0, linecol)   -- N/NE
+	line(68, 62, 127, 37, linecol) -- NE/E
+	line(68, 65, 127, 90, linecol) -- E/SE
+	line(65, 68, 90, 127, linecol) -- SE/S
+	line(62, 68, 37, 127, linecol) -- S/SW
+	line(59, 65, 0, 90, linecol)   -- SW/W
+	line(59, 62, 0, 37, linecol)   -- W/NW
+	line(62, 59, 37, 0, linecol)   -- NW/N
+
+	-- large
+	line(37, 0, 90, 0, linecol)     -- N
+	line(90, 0, 127, 37, linecol)   -- NE
+	line(127, 37, 127, 90, linecol) -- E
+	line(127, 90, 90, 127, linecol) -- SE
+	line(90, 127, 37, 127, linecol) -- S
+	line(37, 127, 0, 90, linecol)   -- SW
+	line(0, 90, 0, 37, linecol)     -- W
+	line(0, 37, 37, 0, linecol)     -- NW
 end
 function _draw()
-	cls(5)
-	circfill(x, y, 7, 14)
-	drawbullets()
+	cls(0)	
+	drawbackground(7)
 end
